a0b6cfd56bb088b1ae81bf69d7adac41292403d2
AJ ONeal <awesome@coolaj86.com>
2015-03-20 00:55:41 -0600

    Create create-root-ca.sh

---
+++ create-root-ca.sh
@@ -0,0 +1,18 @@
+# make directories to work from
+mkdir -p certs/{devices,client,ca,tmp}
+
+# Create your very own Root Certificate Authority
+openssl genrsa \
+  -out certs/ca/my-root-ca.key.pem \
+  2048
+
+# Self-sign your Root Certificate Authority
+# Since this is private, the details can be as bogus as you like
+openssl req \
+  -x509 \
+  -new \
+  -nodes \
+  -key certs/ca/my-root-ca.key.pem \
+  -days 9131 \
+  -out certs/ca/my-root-ca.crt.pem \
+  -subj "/C=US/ST=Utah/L=Provo/O=ACME Signing Authority Inc/CN=example.com"
