52c89d5cfaf8ad36ed483503eb2587cb7f758acb
AJ ONeal <awesome@coolaj86.com>
2015-03-20 01:29:04 -0600

    quotes make me *feel* safe

--- create-and-sign-device-certs.bash
+++ create-and-sign-device-certs.bash
@@ -7,26 +7,26 @@ DEVICE_NAME="${1}"
 FQDN="${2}"
 
 # make directories to work from
-mkdir -p certs/{devices,client,ca,tmp}
+mkdir -p certs/{devices,ca,tmp}
 
 # Create Certificate for this domain,
 openssl genrsa \
-  -out certs/devices/${DEVICE_NAME}.key.pem \
+  -out "certs/devices/${DEVICE_NAME}.key.pem" \
   2048
 
 # Create the CSR
 openssl req -new \
-  -key certs/devices/${DEVICE_NAME}.key.pem \
-  -out certs/tmp/${DEVICE_NAME}.csr.pem \
+  -key "certs/devices/${DEVICE_NAME}.key.pem" \
+  -out "certs/tmp/${DEVICE_NAME}.csr.pem" \
   -subj "/C=US/ST=Utah/L=Provo/O=ACME Service/CN=${FQDN}"
 
 # Sign the request from Device with your Root CA
 openssl x509 \
-  -req -in certs/tmp/${DEVICE_NAME}.csr.pem \
+  -req -in "certs/tmp/${DEVICE_NAME}.csr.pem" \
   -CA certs/ca/my-root-ca.crt.pem \
   -CAkey certs/ca/my-root-ca.key.pem \
   -CAcreateserial \
-  -out certs/devices/${DEVICE_NAME}.crt.pem \
+  -out "certs/devices/${DEVICE_NAME}.crt.pem" \
   -days 9131
 
 # If you already have a serial file, you would use that (in place of CAcreateserial)
